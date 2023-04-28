e18eded2fbf21dd06023b0e2914f0d3f41902b4b
Justin <justin.gebert@student.htw-berlin.de>
2023-04-28 18:44:19 +0200

    add footer and Header as layout for views

--- main.ts
+++ main.ts
@@ -3,15 +3,18 @@ import { Request, Response,} from 'express';
 const homeController = require('./controllers/homeController');
 const app = express();
 const errorController = require("./controllers/errorController");
+const layouts = require("express-ejs-layouts");
 
 app.set("view engine", "ejs"); 
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(layouts)
+app.use(express.static("public"));
 
 app.get('/', (req:Request, res:Response,) => {
-    res.send('FlatShare');
+    res.render("index")
     }
 );
 
