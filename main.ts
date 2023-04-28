c5034147b0ed7b518cc0f85f3133a80e197f7c43
JustinGebertDesk <justin.gebert.berlin@gmail.com>
2023-04-28 10:41:03 +0200

    List Rendering based on Json on /todos Route

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { Request, Response,} from 'express';
+const homeController = require('./controllers/homeController');
 
 
 const app = express();
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,6 +14,8 @@ app.get('/', (req:Request, res:Response) => {
     res.send('FlatShare');
     }
 );
+
+app.get("/todos", homeController.showTodos)
 
 
 app.listen(3000, () => {
