4efb1e2a4b2abe32811df7576fac02c3ca84cd4b
Justin <justin.gebert@student.htw-berlin.de>
2023-04-28 18:32:53 +0200

    fix error issue

--- main.ts
+++ main.ts
@@ -1,26 +1,25 @@
 import express from 'express';
 import { Request, Response,} from 'express';
 const homeController = require('./controllers/homeController');
-
-
 const app = express();
 const errorController = require("./controllers/errorController");
 
 app.set("view engine", "ejs"); 
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 
-app.use(errorController.pageNotFoundError);
-app.use(errorController.internalServerError);
-
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req:Request, res:Response,) => {
     res.send('FlatShare');
     }
 );
 
 app.get("/todos", homeController.showTodos)
+
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
 
 app.listen(3000, () => {
