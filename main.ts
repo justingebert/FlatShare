62451317a3278afbfa393114f028c649b6ee5c77
nadjaml <s0549481@htw-berlin.de>
2023-05-26 19:27:30 +0200

    missing form

--- main.ts
+++ main.ts
@@ -5,14 +5,15 @@ const app = express();
 const errorController = require("./controllers/errorController");
 const layouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const expensesController = require("./controllers/expensesController");
 
+mongoose.Promise= global.Promise
 mongoose.connect(
     "mongodb://localhost:27017/flatshare", 
     {useNewUrlParser: true,}
 );
 
 const db = mongoose.connection;
-
 db.once("open", () => {
     console.log("Successfully connected to MongoDB using Mongoose!");
 })
@@ -25,7 +26,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(layouts)
 app.use(express.static("public"));
 
-app.get('/', homeController.showHome);
+app.get("/expenses",expensesController.getAllExpenses);
+app.get("/addexpenses", expensesController.getExpensesPage);
+app.post("/saveexpenses", expensesController.saveExpense);
 
 app.get("/todos", homeController.showTodos);
 app.get("/chat", (req:Request, res:Response,) => {
