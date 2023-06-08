83885ba86ebb019c636b996e0851820b3db32279
JustinGebertDesk <justin.gebert.berlin@gmail.com>
2023-06-09 00:17:56 +0200

    basic crudd setup for Todo

--- data/repl_testing.js
+++ data/repl_testing.js
@@ -2,6 +2,50 @@ const mongoose = require("mongoose");
 const User = require("./models/user");
 const Todo = require("./models/todo");
 
+// connect to mongodb
+
+
 let testUser, testTodo;
 
 mongoose.Promise = global.Promise;
+mongoose.connect(
+    "mongodb://localhost:27017/flatshare", 
+    {useNewUrlParser: true,}
+);
+
+const db = mongoose.connection;
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+})
+
+
+// test creating a user with a todo
+User.create({
+    name:{
+        first: "test",
+        last: "user",
+    },
+    password: "testpassword",
+    email: "test2@test.com",
+    todos: []
+})
+.then(user => {
+    testUser = user;
+    return Todo.create({
+        name: "test todo",
+        user: user._id
+    })
+})
+.then(todo => {
+    testTodo = todo;
+    testUser.todos.push(todo);
+    return testUser.save();
+})
+.then(() => {
+    console.log("test user and todo created");
+    console.log(testUser);
+})
+.catch(err => {
+    console.log(err);
+})
+
